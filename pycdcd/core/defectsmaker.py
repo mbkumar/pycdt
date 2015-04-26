c2087b0ce614bd7f742e84528297ad4b81067ca8
Danny Broberg <dbroberg@berkeley.edu>
2015-04-26 14:02:34 -0700

    same charge fixes for substitutional defects

--- pycdcd/core/defectsmaker.py
+++ pycdcd/core/defectsmaker.py
@@ -177,16 +177,14 @@ class ChargedDefectsStructures(object):
                 for subspecie_symbol in substitutions[vac_symbol]:
                     sub_sc = vac_sc.copy()
                     sub_sc.append(subspecie_symbol, vac_sc_site.frac_coords)
-                    oxi_min = min(subspecie_symbol[0],0)
-                    oxi_max = max(subspecie_symbol[1]+1,1)
+                    oxi_min = min(max_min_oxi[subspecie_symbol][0]-1,0)
+                    oxi_max = max(max_min_oxi[subspecie_symbol][1]+1,1)
                     sub_defs.append({
                         'name': vac_symbol+str(nb_per_elts[vac_specie])+ \
                                 "_subst_"+subspecie_symbol,
                         'unique_site': vac_site,
                         'supercell':{'size':sc_scale,'structure':sub_sc},
                         'charges':[c-oxi_states[vac_symbol] for c in range(
-                            #max_min_oxi[subspecie_symbol][0],
-                            #max_min_oxi[subspecie_symbol][1]+1)]})
                             oxi_min, oxi_max)]})
 
         self.defects['vacancies'] = vacancies 
