6e4f974142bd85dfa74956ce03e7a67c48e59661
Bharat Medasani <mbkumar@gmail.com>
2015-05-20 11:29:41 -0700

    parse_calculations_new.py renamed to parse_calculations.py

--- pycdcd/utils/parse_calculations.py
+++ pycdcd/utils/parse_calculations.py
@@ -1,11 +1,12 @@
 # coding: utf-8
 """
-Parses the vasprun.xml files generated during VASP defect calculations.
+Parses the vasprun.xml files generated during VASP defect calculations
+in conformation with DefectsAnalyzer written by Geoffroy.
 """
 #from __future__ import unicode_literals
 from __future__ import division
 
-__author__ = "Bharat Medasani, Nils Zimmerman"
+__author__ = "Bharat Medasani, Nils Zimmermann, Danny Broberg"
 __copyright__ = "Copyright 2014, The Materials Project"
 __version__ = "1.0"
 __maintainer__ = "Bharat Medasani"
@@ -17,22 +18,21 @@ import sys
 import glob
 from argparse import ArgumentParser
 
+from monty.serialization import loadfn, dumpfn
+from monty.json import MontyEncoder, MontyDecoder
 from pymatgen.matproj.rest import MPRester
 from pymatgen.io.vaspio.vasp_output import Vasprun
 from pymatgen.electronic_structure.bandstructure import BandStructure
-from pymatgen.phasediagram.pdmaker import PhaseDiagram
-from pymatgen.phasediagram.pdanalyzer import PDAnalyzer
+from pymatgen.entries.computed_entries import ComputedStructureEntry
+from pycdcd.corrections.defects_analyzer import ParsedDefect, DefectsAnalyzer
 
-def parse_defect_energy(structure, root_fldr):
+
+def parse_defect_calculations(root_fldr):
     """
-    Parses the defect energies
+    Parses the defect calculations as ComputedStructureEntries/ParsedDefects.
+    Charge correction is missing in the first run.
     """
-
-    energy_dict = {}
-
-    antisites = []
-    vacancies = []
-    substitutions = []
+    parsed_defects = []
     subfolders = glob.glob(os.path.join(root_fldr,"bulk"))
     subfolders += glob.glob(os.path.join(root_fldr,"vac_*"))
     subfolders += glob.glob(os.path.join(root_fldr,"as_*"))
@@ -72,62 +72,41 @@ def parse_defect_energy(structure, root_fldr):
                 print "Abandoing parsing of the calculations"
                 break
             bulk_energy = vr.final_energy
+            bulk_struct = vr.final_structure
             bulk_sites = vr.structures[-1].num_sites
-            bulk_locpot_path = os.path.abspath(os.path.join(
-                fldr_name,'LOCPOT'))
+            bulk_locpot_path = os.path.abspath(os.path.join(fldr,'LOCPOT'))
+            bulk_entry = ComputedStructureEntry(bulk_struct, bulk_energy, 
+                    data={'locpot_path':bulk_locpot_path})
         else:
             chrg_fldrs = glob.glob(os.path.join(fldr,'charge_*'))
-            energies = {}
-            locpot_paths = {}
             for chrg_fldr in chrg_fldrs:
-                chrg = int(chrg_fldr_name.split("_")[1])
                 vr, error_msg = get_vr_and_check_locpot(chrg_fldr)
                 if error_msg:
                     print (fldr_name, 'charge- ', chrg, error_msg)
                     print "But parsing of the rest of the calculations"
                     continue  # The successful calculations maybe useful
-                energies[chrg] = vr.final_energy
-                locpot_paths[chrg] = os.path.abspath(os.path.join(
-                    chrg_fldr, 'LOCPOT')
-            if 'vac' in fldr_fields:
-                site_index = int(fldr_fields[1])
-                site_multiplicity = int(fldr_fields[2].split("-")[1])
-                site_specie = fldr_fields[3].split("-")[1]
-                vacancies.append({'site_index':site_index,
-                    'site_specie':site_specie,'energies':energies,
-                    'site_multiplicity':site_multiplicity,
-                    'locpot_paths':locpot_paths
-                    })
 
-            else:
-                site_index = int(fldr_fields[1])
-                site_multiplicity = int(fldr_fields[2].split("-")[1])
-                site_specie = fldr_fields[3].split("-")[1]
-                substitution_specie = fldr_fields[4].split("-")[1]
-                dict_def = {'site_index':site_index,
-                        'site_specie':site_specie,'energies':energies,
-                        'substitution_specie':substitution_specie,
-                        'site_multiplicity':site_multiplicity,
-                        'locpot_paths':locpot_paths
-                        }
-                if 'as' in fldr_fields:
-                    antisites.append(dict_def)
-                else:
-                    substitutions.append(dict_def)
-    else:
-        print "All calculations successful for ", mpid
-        vacancies.sort(key=lambda entry: entry['site_index'])
-        antisites.sort(key=lambda entry: entry['site_index'])
-        substitutions.sort(key=lambda entry: entry['site_index'])
-        energy_dict[unicode(mpid)] = {u"structure":structure,
-                u'bulk_supercell_energy':bulk_energy,
-                u'bulk_supercell_sites':bulk_sites,
-                u'vacancies':vacancies,
-                u'antisites':antisites,
-                u'substitutions':substitutions}
-        return energy_dict
+                #chrg = int(chrg_fldr_name.split("_")[1])
+                trans_dict = loadfn('transformation.json')
+                chrg = trans_dict['charge']
+                site = trans_dict['defect_supercell_site']
+                energy = vr.final_energy
+                struct = vr.final_structure
+                encut = vr.incar['ENCUT']
+                locpot_path = os.path.abspath(os.path.join(chrg_fldr, 'LOCPOT'))
+                parsed_defects.append(ParsedDefect(
+                    ComputedStructureEntry(struct, energy, 
+                        data={'locpot_path':locpot_path,'encut':encut}),
+                    site_in_bulk=site, charge=chrg, name=fldr_name))
+
+    else: 
+        parsed_defects_data = {}
+        parsed_defects_data['bulk_entry'] = bulk_entry 
+        parsed_defects_data['defects'] = parsed_defects 
+        return parsed_defects_data
 
     return {} # Return Null dict due to failure
+
 
 def get_vbm(mpid, mapi_key=None):
     """
@@ -160,24 +139,27 @@ def get_atomic_chempots(structure):
 
     note: could also do this with mpid if that would be easier..
     """
-    specs=list(set(structure.species))
-    listspec=[i.symbol for i in specs]
-    print 'look for atomic chempots relative to:',listspec
+    #specs=list(set(structure.species))
+    #listspec=[i.symbol for i in specs]
+    #print 'look for atomic chempots relative to:',listspec
+    species = [specie.symbol for specie in structure.types_of_speice]
     
     mp=MPRester() 
-    entries=mp.get_entries_in_chemsys(listspec)
-    if len(listspec)==1:
+    entries=mp.get_entries_in_chemsys(species)
+    if len(species)==1:
         print 'this is elemental system! use bulk value.'
-        vals=[i.energy_per_atom for i in entries]
+        vals=[entry.energy_per_atom for entry in entries]
         chempot={specs[0]:min(vals)}
         return chempot
+
     pd=PhaseDiagram(entries)
-    print pd
+    #print pd
 
     chemlims={}
     for i in specs:
         name=str(i)+'-limiting'
-        tmpchemlims=PDAnalyzer(pd).get_chempot_range_stability_phase(self._structure.composition,i)
+        tmpchemlims=PDAnalyzer(pd).get_chempot_range_stability_phase(
+                self._structure.composition,i)
         chemlims[name]={str(i)+'-rich':{},str(i)+'-poor':{}}
         for j in tmpchemlims.keys():
             chemlims[name][str(i)+'-rich'][j]=tmpchemlims[j][1]
@@ -185,8 +167,49 @@ def get_atomic_chempots(structure):
 
     #make this less confusing for binary systems...
     if len(specs)==2:
-	chemlims=chemlims[chemlims.keys()[0]]
+	    chemlims=chemlims[chemlims.keys()[0]]
 
     return chemlims 
 
+
+def parse_dielectric_calculation_for_tensor(root_fldr):
+    """
+    Parses the "vasprun.xml" file in subdirectory "dielec" of root
+    directory root_fldr and returns the dielectric tensor.
+
+    Args:
+        root_fldr (str):
+            root directory where subdirectory "dielec" is expected
+    Returns:
+        eps (3x3 float matrix):
+            dielectric tensor
+    """
+
+    vrun = Vasprun(os.path.join(root_fldr,"dielectric","vasprun.xml"))
+    eps_ion = vrun.epsilon_ionic
+    eps_stat = vrun.epsilon_static
+
+    eps = []
+    for i in range(len(eps_ion)):
+        eps.append([e[0]+e[1] for e in zip(eps_ion[i],eps_stat[i])])
+    
+    return eps
+
+def parse_dielectric_calculation(root_fldr):
+    """
+    Parses the "vasprun.xml" file in subdirectory "dielec" of root
+    directory root_fldr and returns the average of the trace
+    of the dielectric tensor.
+
+    Args:
+        root_fldr (str):
+            root directory where subdirectory "dielec" is expected
+    Returns:
+        eps (float):
+            average of the trace of the dielectric tensor
+    """
+
+    eps_ten = parse_dielectric_calculation_for_tensor(root_fldr)
+
+    return (eps_ten[0][0]+eps_ten[1][1]+eps_ten[2][2])/3.0
 
