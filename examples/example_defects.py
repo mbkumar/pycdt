bc8aa18bc9dc5cd7e1e16887e74b723473e9ac86
Bharat Medasani <mbkumar@gmail.com>
2017-02-17 13:18:39 -0800

    POTCAR change demo in example

--- examples/example_defects.py
+++ examples/example_defects.py
@@ -2,6 +2,7 @@ __author__ = 'geoffroy'
 
 from pymatgen.core.structure import Structure
 from pymatgen.core.periodic_table import Element
+from monty.serialization import loadfn
 from pycdt.core.defectsmaker import ChargedDefectsStructures
 from pycdt.utils.vasp import make_vasp_defect_files, \
         make_vasp_dielectric_files
@@ -10,7 +11,7 @@ import json
 
 def example_maker():
         struct=Structure.from_dict(json.loads(open("PbTiO3.json",'r').read()))
-        make_vasp_dielectric_files(struct)
+        make_vasp_dielectric_files(struct, user_settings=loadfn('vasp_settings.yaml'))
         make_vasp_defect_files(ChargedDefectsStructures(struct,
             max_min_oxi={'Pb':(0,2),'Ti':(0,4),'O':(-2,0),'Al':(3,4),'V':(3,4),
                 "Cr":(3,4),'Ga':(3,4),'Fe':(3,4),'Co':(3,4),'Ni':(3,4),
@@ -18,7 +19,8 @@ def example_maker():
             substitutions={'Pb':['Na','K'],
                 'Ti':['Al','V','Cr','Ga','Fe','Co','Ni'],'O':['N']}, 
             oxi_states={'Pb':2,'Ti':4,'O':-2}).defects, 
-            struct.composition.reduced_formula)
+            struct.composition.reduced_formula,
+            user_settings=loadfn('vasp_settings.yaml'))
 
 if __name__ == '__main__':
     example_maker()
