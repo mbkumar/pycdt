05784164e1621ca4a9829cb41f4f3a26aaf1fd1c
dbroberg <dbroberg@berkeley.edu>
2019-05-08 12:27:13 -0700

    updates to finite_size_charge_correction and unit test

--- pycdt/corrections/tests/test_finite_size_charge_correction.py
+++ pycdt/corrections/tests/test_finite_size_charge_correction.py
@@ -10,57 +10,88 @@ __email__ = "mbkumar@gmail.com"
 __status__ = "Development"
 __date__ = "Jan 26, 2016"
 
-import os
 import unittest
+import numpy as np
 
-from pymatgen.io.vasp.outputs import Locpot
-from pymatgen.entries.computed_entries import ComputedStructureEntry
+from pymatgen.core.sites import PeriodicSite
+from pymatgen.util.testing import PymatgenTest
+from pymatgen.analysis.defects.core import DefectEntry, Vacancy
 
-from pycdt.corrections.finite_size_charge_correction import *
-from pycdt.core.defects_analyzer import ComputedDefect
+from pycdt.corrections.finite_size_charge_correction import get_correction_freysoldt, get_correction_kumagai
 
-#Paths to locpots we are testing on
-bl_path = os.path.abspath(os.path.join(
-    __file__, '..', '..', '..', '..', 'test_files', 'bLOCPOT.gz'))
-dl_path = os.path.abspath(os.path.join(
-    __file__, '..', '..', '..', '..', 'test_files', 'dLOCPOT.gz'))
 
-class FiniteSizeChargeCorrectionTest(unittest.TestCase):
+class FiniteSizeChargeCorrectionTest(PymatgenTest):
     """
-    Test functions for getting freysoldt and kumagai as well as ChargeCorrection class itself
-    if one breaks the other will likely break as well...
+    Test functions for getting freysoldt and kumagai corrections
     """
     def setUp(self):
-        self.bl=Locpot.from_file(bl_path)
-        self.dl=Locpot.from_file(dl_path)
-        self.bs=self.bl.structure
-        self.ds=self.dl.structure
-        # self.bulk_entry = ComputedStructureEntry(
-        #         self.bs, 100, data={'locpot_path': bl_path, 'encut': 520})
-        # self.defect = ComputedStructureEntry(
-        #         self.ds, 100, data={'locpot_path': dl_path, 'encut': 520,
-        #                             'charge': -3})
-        # self.computed_defect = ComputedDefect(
-        #         self.defect_entry, self.bs.sites[0], charge=-3,
-        #         name='vac_1_Ga')
-        # self.kbi = KumagaiBulkInit(self.bs, self.bl.dim, 15,
-        #                            optgamma=3.49423226983)
 
-        #TODO: update this unit test
+        self.epsilon = 15.
+
+        struc = PymatgenTest.get_structure("VO2")
+        struc.make_supercell(3)
+        vac = Vacancy(struc, struc.sites[0], charge=-3)
+
+        # load neccessary parameters for defect_entry to make use
+        # of Freysoldt and Kumagai corrections
+        p = {}
+        ids = vac.generate_defect_structure(1)
+        abc = struc.lattice.abc
+        axisdata = [np.arange(0., lattval, 0.2) for lattval in abc]
+        bldata = [np.array([1. for u in np.arange(0., lattval, 0.2)]) for lattval in abc]
+        dldata = [
+            np.array([(-1 - np.cos(2 * np.pi * u / lattval)) for u in np.arange(0., lattval, 0.2)]) for lattval in abc
+        ]
+        p.update({'axis_grid': axisdata, 'bulk_planar_averages': bldata, 'defect_planar_averages': dldata,
+                  'initial_defect_structure': ids, 'defect_frac_sc_coords': struc.sites[0].frac_coords,
+                  'bulk_sc_structure': struc})
+
+        bulk_atomic_site_averages, defect_atomic_site_averages = [], []
+        defect_site_with_sc_lattice = PeriodicSite( struc.sites[0].specie, struc.sites[0].coords,
+                                                    struc.lattice, coords_are_cartesian = True)
+        max_dist = 9.6
+        pert_amnt = 1.
+        for site_ind, site in enumerate(struc.sites):
+            if site.specie.symbol == "O":
+                Oval = -30.6825
+                bulk_atomic_site_averages.append( Oval)
+                if site_ind:
+                    dist_to_defect = site.distance_and_image( defect_site_with_sc_lattice)[0]
+                    defect_site_val = Oval - .3 + pert_amnt * ((max_dist - dist_to_defect)/max_dist)**2
+                    defect_atomic_site_averages.append( defect_site_val)
+            else:
+                Vval = -51.6833
+                bulk_atomic_site_averages.append( Vval)
+                if site_ind:
+                    dist_to_defect = site.distance_and_image( defect_site_with_sc_lattice)[0]
+                    defect_site_val = Vval - .3 + pert_amnt * ((max_dist - dist_to_defect)/max_dist)**2
+                    defect_atomic_site_averages.append( defect_site_val)
+
+        site_matching_indices = [[ind, ind-1] for ind in range(len(struc.sites)) if ind != 0]
+
+        p.update({ "bulk_atomic_site_averages": bulk_atomic_site_averages,
+                   "defect_atomic_site_averages": defect_atomic_site_averages,
+                   "site_matching_indices": site_matching_indices})
+        self.defect_entry = DefectEntry( vac, 0., parameters = p)
 
     def test_get_correction_freysoldt(self):
-        # freyout = get_correction_freysoldt(self.computed_defect,
-        #                                    self.bulk_entry, 15)
-        # self.assertAlmostEqual(freyout[0], 3.99126)
-        # self.assertIsInstance(freyout[1], Locpot)
-        pass
+        freyout = get_correction_freysoldt( self.defect_entry, self.epsilon,
+                                            title = None, partflag='All', axis=None)
+        self.assertEqual( freyout, 5.445950368792991)
+
+        freyout = get_correction_freysoldt( self.defect_entry, self.epsilon,
+                                            title = None, partflag='AllSplit', axis=None)
+        self.assertEqual( freyout[0], 0.975893)
+        self.assertEqual( freyout[1], 4.4700573687929905)
+        self.assertEqual( freyout[2], 5.445950368792991)
 
     def test_get_correction_kumagai(self):
-        # kumagaiout = get_correction_kumagai(self.computed_defect, './',
-        #                                     self.kbi, bulk_locpot=self.bl)
-        # self.assertAlmostEqual(kumagaiout, 4.24073)
-        pass
+        kumagaiout = get_correction_kumagai( self.defect_entry, self.epsilon,
+                                            title = None, partflag='AllSplit')
+        self.assertEqual( kumagaiout[0], 0.9763991294314076)
+        self.assertEqual( kumagaiout[1], 0.2579750033409367)
+        self.assertEqual( kumagaiout[2], 1.2343741327723443)
 
-import unittest
+
 if __name__ == '__main__':
     unittest.main()
