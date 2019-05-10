6f4e6bf589a6cb3c812c23f00b83fd6aded192c6
dbroberg <dbroberg@berkeley.edu>
2019-05-10 10:07:43 -0700

    update to setup file

--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ setup(
         classifiers=[
             "Programming Language :: Python :: 2",
             "Programming Language :: Python :: 2.7",
-            #"Programming Language :: Python :: 3.4",
-            "Development Status :: 1 - Alpha",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
             "Intended Audience :: Science/Research",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
